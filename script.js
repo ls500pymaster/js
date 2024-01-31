48bd1e8b720c63bb06b901dead11950524b25a3c
Aleksandr <94639744+ls500pymaster@users.noreply.github.com>
2024-02-01 00:33:22 +0300

    First commit

--- script.js
+++ script.js
@@ -4,24 +4,14 @@ const fingerNumber = +prompt("Type number of finger");
 
 if (fingerNumber === 1) {
     alert("Thumb");
-}
-
-else if (fingerNumber === 2) {
+} else if (fingerNumber === 2) {
     alert("Pointer");
-}
-
-else if (fingerNumber === 3) {
+} else if (fingerNumber === 3) {
     alert("Middle");
-}
-
-else if (fingerNumber === 4) {
+} else if (fingerNumber === 4) {
     alert("Ring");
-}
-
-else if (fingerNumber === 5) {
+} else if (fingerNumber === 5) {
     alert("Little");
-}
-
-else {
+} else {
     alert("Enter number from 1 to 5");
 }
