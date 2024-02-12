e4cb27f704a620e800e21275835e3875604c1c6b
Aleksandr <94639744+ls500pymaster@users.noreply.github.com>
2024-02-12 23:16:51 +0300

    remove element with new element

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const arr = [2, 4, 5, 10, 22];
 
 function removeElement(arr, elem, newElem) {
-    let found = false
+    let found = false;
     for (let i = 0; i < arr.length; i++) {
-        console.log(elem)
+        console.log(elem);
         if (arr[i] === elem) {
             arr.splice(i, 1, newElem);
             found = true;
@@ -15,4 +15,4 @@ function removeElement(arr, elem, newElem) {
     }
 }
 removeElement(arr, 2, 777);
-console.log('arr ->', arr)
+console.log('arr ->', arr);
