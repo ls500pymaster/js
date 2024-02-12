80c740f131cc1c1e0efe176d39674b17389ebb8b
Aleksandr <94639744+ls500pymaster@users.noreply.github.com>
2024-02-12 23:15:17 +0300

    remove element with new element

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-// Leap year
+const arr = [2, 4, 5, 10, 22];
 
-const leapYear = prompt("Please input year");
-
-if (leapYear % 400 === 0) {
-    alert("Leap year");
-} else if (leapYear % 4 === 0 && leapYear % 100 !== 0) {
-    alert("Leap year");
-} else {
-    alert("Not a leap year");
+function removeElement(arr, elem, newElem) {
+    let found = false
+    for (let i = 0; i < arr.length; i++) {
+        console.log(elem)
+        if (arr[i] === elem) {
+            arr.splice(i, 1, newElem);
+            found = true;
+            console.log(arr);
+        }
+    }
+    if (found === false) {
+        return arr;
+    }
 }
+removeElement(arr, 2, 777);
+console.log('arr ->', arr)
