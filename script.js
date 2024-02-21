b9127a924bbf7bbfc44355a77769468145326a33
Aleksandr <94639744+ls500pymaster@users.noreply.github.com>
2024-02-21 23:46:33 +0300

    test commit

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-const newArr = [false, 12, '23424', false, 324, '', true, null, 5, -0, 0, 9, undefined, NaN, 6.354, undefined, 7, null];
+const car = {
+    'color': "Black",
+    'brand': "Lexus",
+    'model': "GS",
+    'engine': "V6",
+    'markLewinson' : undefined,
+};
 
-const result = newArr.filter((elem) => Boolean(elem));
 
-console.log(result);
+console.log('engine' in car);
+
+for (let key in car) {
+
+    const val = car[key];
+    console.log("key ->", key);
+    console.log("value ->", val);
+}
