ac9004682302ae49134f8e3676edbdcbdb688d18
Aleksandr <94639744+ls500pymaster@users.noreply.github.com>
2024-02-27 21:22:36 +0300

    Object new user and properties (for in)

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-function processNumber(num1, num2) {
-    if (num1 % 2 === 0 && num2 % 2 === 0) {
-        return num1* num2;
-    }
-    else if (num1 % 2 !== 0 && num2 % 2 !== 0) {
-        return num1 + num2;
-    }
-    else {
-        return num1 % 2 !== 0 ? num1 : num2;
-    }
+const firstName = prompt("What is your name?");
+const age = parseInt(prompt("What is your age?"));
+const email = prompt("What is your email?");
+
+
+const newUser = {
+    firstName: firstName,
+    age: age,
+    email: email,
 }
 
-console.log(processNumber(6, 8));
-console.log(processNumber(3, 5));
-console.log(processNumber(10, 7));
+for (const property in newUser) {
+    console.log(`${property}: ${newUser[property]}`);
+}
+
+
+
