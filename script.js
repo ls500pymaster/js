82fbaef9b3c12f864ecdbbf9f8c6997834615b9b
Aleksandr <94639744+ls500pymaster@users.noreply.github.com>
2024-03-17 19:42:48 +0200

    simple notepad js

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
-name = prompt("Input your name:");
+const list = document.createElement("ul");
+const tasks = [];
 
-say_hello = console.log("Hello,", name,"!",  "How are you?");
+function startNotepad() {
+    addTask();
+    createList(tasks);
+    document.body.append(list);
+}
 
-console.log(say_hello);
+function addTask() {
+    while (true) {
+        const task = prompt("Enter your task");
+        if (task === null) {
+            break;
+        }
+        tasks.push(task);
+    }
+}
+
+function createList(tasks) {
+    tasks.forEach((task) => {
+        console.log(task);
+        const listItem = document.createElement("li");
+        listItem.textContent = task;
+        list.append(listItem);
+    })
+}
+
+startNotepad();
+
