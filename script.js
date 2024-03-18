c41ff9f626a9590606117dc4e482eb9562c65ee5
Aleksandr <94639744+ls500pymaster@users.noreply.github.com>
2024-03-18 22:24:29 +0200

    js get location and generate ext. url

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-name = prompt("Input your name:");
+function getLocation() {
+    navigator.geolocation.getCurrentPosition(position => {
+        const latitude= position.coords.latitude;
+        const longitude= position.coords.longitude;
 
-say_hello = console.log("Hello,", name,"!",  "How are you?");
+        const myLat = document.createElement("div");
+        const myLong = document.createElement("div");
+        myLong.textContent = `My Longitude: ${longitude}`;
+        myLat.textContent = `My Latitude: ${latitude}`;
 
-console.log(say_hello);
+        document.body.append(myLong);
+        document.body.append(myLat);
+
+        const newLink = document.createElement("a");
+        newLink.href = `https://www.google.com/maps/place/${latitude},${longitude}`;
+        newLink.textContent = "Your location";
+        document.body.append(newLink);
+    });
+}
+
+getLocation();
+
+
+
