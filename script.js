253c04cb742bd054fc8536d8d4aeef812d23ab91
Aleksandr <94639744+ls500pymaster@users.noreply.github.com>
2024-03-18 21:43:43 +0200

    js cookies

--- script.js
+++ script.js
@@ -1,5 +1,44 @@
-name = prompt("Input your name:");
+function setCookie(name, value, days) {
+    const date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    const expires = `expires=${date.toUTCString()}`;
+    document.cookie = `${name}=${value}; ${expires}; path=;`
+    console.log()
 
-say_hello = console.log("Hello,", name,"!",  "How are you?");
+}
 
-console.log(say_hello);
+setCookie('cat', 'silver', 3);
+
+
+function getCookie(cookie) {
+    const cookies = document.cookie.split('; ')
+    console.log(cookies)
+    for (let i =0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim();
+
+        // cookie name = cookie value
+
+        if (cookie.indexOf(name + '=') === 0) {
+            return cookie.substring(name.length + 1);
+        }
+    }
+}
+
+function hasCookie(cookie) {
+    const cookies = document.cookie.split(';');
+
+    for (let i =0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim();
+
+        // cookie name = cookie value
+
+        if (cookie.indexOf(name + '=') === 0) {
+            return true
+        }
+    }
+    return false;
+}
+
+function removeCookie(cookie) {
+    document.cookie = `${name}=; expires=Mon, 18 Mar 1980 18:21:36 GMT; path=/`
+}
